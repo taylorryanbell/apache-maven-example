afdb7a28d8e67950e7f7b3f6b3354a37966a9b73
Taylor Bell <taylor.ryan.bell@gmail.com>
2022-01-18 09:37:35 -0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  Simple Apache Maven & Beam Example  ");
+        System.out.println("--------------------------------------");
+        
+    }
+}
